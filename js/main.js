3c09d2c4e11f692ece79f0f85e71089801f19952
lunagorojovsky <lunagoro@gmail.com>
2026-09-17 12:27:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ const proyectos = document.querySelectorAll('.proyecto');
 proyectos.forEach((p, i) => {
   p.style.opacity = '0';
   p.style.transform = 'translateY(12px)';
-  p.style.transition = 'opacity 0.9s ease, transform 0.9s ease';
+  p.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
   setTimeout(() => {
     p.style.opacity = '1';
     p.style.transform = 'translateY(0)';
-  }, 100 + i * 150);
+  }, 50 + i * 50);
 });
