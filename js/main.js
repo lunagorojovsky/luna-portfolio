fe7c6775e009040b2a6e8ac3fad1a16d543960a5
lunagorojovsky <lunagoro@gmail.com>
2026-09-17 12:27:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 const proyectos = document.querySelectorAll('.proyecto');
 proyectos.forEach((p, i) => {
   p.style.opacity = '0';
-  p.style.transform = 'translateY(12px)';
+  p.style.transform = 'translateY(6px)';
   p.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
   setTimeout(() => {
     p.style.opacity = '1';
